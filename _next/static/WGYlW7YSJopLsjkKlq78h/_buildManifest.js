8c87dab2721deb10f86387b6da2a657e53928fd3
trian annas <tatstrian@gmail.com>
2021-12-14 16:37:43 +0700

    bug fix

---
+++ _next/static/WGYlW7YSJopLsjkKlq78h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/200-302412783dca8c4f.js","static/css/6d810e3f856d5ee3.css","static/chunks/pages/index-ae6b62978e68a558.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
